a6499c151f63851120899dcc4d62149fe9249986
Hiep Vu <vuhiep111166@gmail.com>
2022-01-20 21:39:22 +0700

    Joystick và Nav-mesh [Đang xử lý]

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,7 +7,7 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] private float spd;
-    [SerializeField] private DynamicJoystick _dynamicJoystick;
+    [SerializeField] private DynamicJoystick dynamicJoystick;
     private Vector3 mousePos;
     private void Start()
     {
@@ -16,13 +16,17 @@ public class Player : MonoBehaviour
 
     private void Update()
     {
-        if (!_dynamicJoystick) return;
-        mousePos = Input.mousePosition;
-        var v = Input.mousePosition - mousePos;
-        if(v.magnitude<=0) return;
-        v = v.normalized;
-        v *= spd * Time.smoothDeltaTime;
+        if (Input.GetMouseButtonDown(0))
+        {
+            mousePos = Input.mousePosition;
+        }
+        if (Input.GetMouseButton(0))
+        {
+            var v = Input.mousePosition - mousePos;
+            if(v.magnitude<=0) return;
+            v = v.normalized;
+            v *= spd * Time.smoothDeltaTime;
         transform.position += new Vector3(v.x, 0, v.y);
-
+        }
     }
 }
